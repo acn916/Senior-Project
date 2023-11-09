e0f685a75c2faa2f3d450435c7c7e3cf02f96197
Laura Kay <laurakay@Lauras-MacBook-Pro.local>
2023-11-08 21:51:03 -0800

    JIRA/DT-26,DT-79,DT-128 Completed the Services page including buttons and table of all available services

--- frontend/fresh-cuts/src/App.js
+++ frontend/fresh-cuts/src/App.js
@@ -1,9 +1,11 @@
 import Home from "./scenes/home";
+import Services from "./scenes/services";
 
 function App() {
   return (
     <div className="App">
       <Home/>
+      <Services/>
     </div>
   );
 }
