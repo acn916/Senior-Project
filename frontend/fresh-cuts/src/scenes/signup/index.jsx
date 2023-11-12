3a2aaae0cc95c434f73e6e1d15f4fe2769ac227b
Ben <voben21@gmail.com>
2023-11-11 18:02:21 -0800

    DT-118 REST API needs to be created to create a new account, Implemented creating an account and verifying it using AWS Cognito

--- frontend/fresh-cuts/src/scenes/signup/index.jsx
+++ frontend/fresh-cuts/src/scenes/signup/index.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, {useState} from 'react';
 import {
 Grid, Paper, TextField, FormControlLabel, FormGroup, Checkbox, Button, Typography
-} from '@mui/material'
+} from '@mui/material';
+import UserPool from "./UserPool.js";
 
 function phoneFormat(input) {
     input = input.replace(/\D/g,'').substring(0,10);
@@ -13,7 +14,43 @@ function phoneFormat(input) {
 }
 
 export default function Signup() {
+    const [isStylist, setIsStylist] = useState(false); // Updated state for the checkbox
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [phoneNumber, setPhoneNumber] = useState("");
     const paperStyle={padding:20, height:'620px', width:350, margin:"10px auto"}
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+        const attributes = [
+            {
+                Name: "given_name", // First Name attribute
+                Value: firstName,
+            },
+            {
+                Name: "family_name", // Last Name attribute
+                Value: lastName,
+            },
+            {
+                Name: "phone_number", // Phone Number attribute
+                Value: phoneNumber,
+            },
+            {
+                Name: "custom:user_role", // Custom role attribute
+                Value: isStylist ? "Stylist" : "Customer",
+            },
+            // Add more attributes as needed
+        ];
+        UserPool.signUp(email, password, attributes, null, (err, data) => {
+            if (err) {
+                console.error(err);
+            }
+            console.log(data);
+            alert("Please check your email to verify your account.");
+        });
+    };
 
     return (
         <>
@@ -29,18 +66,22 @@ export default function Signup() {
                 Sign In
             </Button>
         </Grid>
-        
-        <Grid>
+
+        <div>
+            <form onSubmit={onSubmit}>
+            <Grid container direction="row" justifyContent="right" alignItems="right">
             <Paper elevation={2} style={paperStyle}>
                 <Grid container direction="row" justifyContent="center" alignItems="center">
                     <h2>Sign Up</h2>
                 </Grid>
-
+                
                 <TextField
                     required id="first-name-signup"
                     label="First Name"
                     margin="normal"
                     fullWidth
+                    value={firstName}
+                    onChange={(event) => setFirstName(event.target.value)}
                 />
 
                 <TextField
@@ -48,13 +89,16 @@ export default function Signup() {
                     label="Last Name"
                     margin="normal"
                     fullWidth
+                    value={lastName}
+                    onChange={(event) => setLastName(event.target.value)}
                 />
 
                 <TextField
                     required id="phone-number-signup"
                     label="Phone Number"
                     margin="normal"
-                    onInput={(e) => { e.target.value = phoneFormat(e.target.value); }}
+                    value={phoneNumber}
+                    onChange={(event) => setPhoneNumber(event.target.value)}
                     fullWidth
                 />
 
@@ -63,6 +107,8 @@ export default function Signup() {
                     label="Email Address"
                     margin="normal"
                     fullWidth
+                    value={email}
+                    onChange={(event) => setEmail(event.target.value)}
                 />
 
                 <TextField
@@ -72,25 +118,34 @@ export default function Signup() {
                     type="password"
                     autoComplete="current-password"
                     fullWidth required
+                    value={password}
+                    onChange={(event) => setPassword(event.target.value)}
                 />
 
                 <FormGroup>
                     <FormControlLabel control={<Checkbox defaultChecked color='default' size='small'/>}
                                       label={<Typography variant='caption'>I agree to the Terms of Service and Privacy Policy.</Typography>}/>
-                    <FormControlLabel control={<Checkbox defaultChecked color='default' size='small'/>}
+                    
+                    <FormControlLabel control={<Checkbox type="checkbox" checked={isStylist}
+                                      onChange={(event) => setIsStylist(event.target.checked)}
+                                      defaultChecked color='default' size='small'/>}
                                       label={<Typography variant='caption'>I am a stylist.</Typography>}/>
                 </FormGroup>
 
                 <Button variant='Contained'
+                        type='submit'
                         style={{
                         backgroundColor: "#e95252",
-                        padding: "12px 24px"
+                        padding: "12px 24px",
                         }}
                         fullWidth required>
                     <Typography color='white'>Create an Account</Typography>
                 </Button>
-            </Paper>
-        </Grid>
+                
+                </Paper>
+                </Grid>
+            </form>
+        </div>
         </>
     )
 }
