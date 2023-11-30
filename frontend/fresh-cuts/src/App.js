bc56eeb5b1016308fbf1fc970139d534bed762a2
bdam98 <brandondam10@gmail.com>
2023-11-29 19:42:13 -0800

    fixed conflicts

--- frontend/fresh-cuts/src/App.js
+++ frontend/fresh-cuts/src/App.js
@@ -7,8 +7,11 @@ import {Route, Routes} from "react-router";
 import Login from "./scenes/login/index.jsx";
 import Signup from "./scenes/signup/index.jsx";
 import Passwordreset from "./scenes/passwordreset/index.js";
+import Staff from "./scenes/staff/index.jsx"
 import { Account } from "./scenes/login/Account.js";
 import ErrorPage from "./scenes/404/index.jsx";
+import Requestsummary from "./scenes/requestsummary/index.jsx";
+import Confirm from "./scenes/confirm/index.jsx";
 import PrivateRoutes from "./PrivateRoutes.js";
 
 function App() {
@@ -21,9 +24,13 @@ function App() {
             <Route element = {<PrivateRoutes />}>
               <Route path="/" element={<Home />} />
               <Route path="/Home" element={<Home />} />
-              {/*<Route path="/Staff" element={<Staff />} />*/}
+
+              <Route path="/Staff" element={<Staff />} />
+              <Route path="/Confirmation" element={<Confirm/>}/>
+
               <Route path="/Services" element={<Services />} />
               <Route path="/Booking" element={<Booking />} />
+              <Route path="/Summary" element={<Requestsummary/>}/>
             </Route>
             <Route path="/Signup" element={<Signup/>}/>
             <Route path="/Login" element={<Account><Login/></Account>}/>
---
+++ frontend/fresh-cuts/src/scenes/requestsummary/index.jsx
@@ -0,0 +1,134 @@
+import React from 'react'
+import {
+    Grid, Paper, TextField, FormControlLabel, FormGroup, Checkbox, Button, Typography, List, ListItem, Divider, ListItemText, ListItemAvatar, Avatar, Container
+    } from '@mui/material';
+import { Link } from 'react-router-dom'
+
+export default function Requestsummary() {
+    const paperStyle={ maxHeight:'80vh', overflowY:'auto', width:'90%', maxWidth:'350px', margin:"30px auto"}
+    const reqStyle={display: 'flex', flexDirection: 'column', width:'90%', maxWidth:'450px', padding:20, margin:"30px auto"}
+
+    return (
+        <>
+        <Container>
+            <Grid container direction="row">
+                <Paper elevation={2} style={reqStyle}>
+                    <Grid container direction="column">
+                        <Typography variant='h5' margin='normal'>
+                            Client Information
+                        </Typography>
+                        <TextField required id="reqsum-firstname" label="First Name" margin='normal'/>
+                        <TextField required id="reqsum-lastname" label="Last Name" margin='normal'/>
+                        <TextField required id="reqsum-phone" label="Phone Number" margin='normal'/>
+                        <TextField required id="reqsum-email" label="Email" margin='normal'/>
+
+                        <Typography variant='h5' margin='normal'> About your appointment</Typography>
+                        <TextField
+                        id="reqsum-specialrequest"
+                        multiline
+                        rows={4}
+                        margin='normal'
+                        label="Do you have any special requests or ideas to share with your service provider?(optional)"
+                        />
+                        
+                        <Typography variant='h5' margin='normal'>Cancellation Policy</Typography>
+                        <Typography margin='normal' marginBottom='20px'>
+                            We are sorry to not see you for your appointment.
+                            Please call our salon at (916) 451-1517 to cancel or move
+                            an appointment so we can give your spot to someone on the waitlist.
+                        </Typography>
+
+                        <Typography variant='h5' margin='normal'>Fine Print</Typography>
+                        <Typography>• Request Appointments are pending for Service Provider Acceptance.</Typography>
+                        <Typography marginBottom='20px'>• Prices and Duration are starting point quotes.</Typography>
+                        
+                        <FormControlLabel 
+                                control={
+                                    <Checkbox 
+                                        type="checkbox" 
+                                        color='default' 
+                                        size='small'
+                                    />}
+
+                                label={
+                                <Typography variant='caption' margin='normal' marginBottom='8px'>
+                                    By clicking Request, you agree to the Cancellation Policy and conditions of this business.
+                                </Typography>}
+                        />
+
+                        <Grid container direction='row' justifyContent="space-between" alignItems="center">
+                            <Button type='submit'
+                                    style={{
+                                        backgroundColor: "#a6a6a6",
+                                        padding: "12px 36px",
+                                        margin: "5px 10px"
+                                    }}>
+                                    <Typography color='white'>Back</Typography>
+                            </Button>
+
+                            <Button variant='Contained'
+                                    type='submit'
+                                    style={{
+                                    backgroundColor: "#e95252",
+                                    padding: "12px 36px",
+                                    margin: "5px 10px"
+                                    }}>
+                                <Typography color='white'>Request</Typography>
+                            </Button>
+                        </Grid>
+                    </Grid>
+                </Paper>
+                
+                <Paper elevation={2} style={paperStyle}>
+                    <Typography
+                        variant='h5' 
+                        margin='normal' 
+                        padding='15px'
+                        align='center' 
+                        color='white'
+                        style={{backgroundColor: "#e95252"}}>
+                        Service Summary
+                    </Typography>
+                    
+                    <List container="true">
+                        <ListItem>
+                            <ListItemText>Date</ListItemText>
+                            <ListItemText>
+                                <Typography align="right">
+                                    Time
+                                </Typography>
+                            </ListItemText>
+                        </ListItem>
+
+                        <Divider/>
+                        
+                        <ListItem>
+                            <ListItemAvatar><Avatar></Avatar></ListItemAvatar>
+                            <ListItemText>Stylist</ListItemText>
+                            <ListItemText>
+                                <Typography align="right">
+                                    Service Price
+                                </Typography>
+                            </ListItemText>
+                        </ListItem>
+
+                        <Divider/>
+                        
+                        <ListItem>
+                            <ListItemText>Total</ListItemText>
+                            <ListItemText>
+                                <Typography align="right">
+                                    Total Price
+                                </Typography>
+                            </ListItemText>
+                        </ListItem>
+                    </List>
+                </Paper>
+            </Grid>
+
+
+        </Container>
+        
+        </>
+    )
+}
