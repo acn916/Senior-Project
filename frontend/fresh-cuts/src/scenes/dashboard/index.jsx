a3adeda6b360904ce57c5c2e899a0f6b3f8b1bc6
acn916 <austin.n916@gmail.com>
2024-02-07 19:10:31 -0800

    JIRA DT-92/DT-194 GET request to retrieve all stylist and created the drop down menu displaying the salon stylist

--- frontend/fresh-cuts/src/scenes/dashboard/index.jsx
+++ frontend/fresh-cuts/src/scenes/dashboard/index.jsx
@@ -1,5 +1,7 @@
 import {useState, useEffect} from 'react'
+import axios from "axios";
 
+import { Paper } from '@mui/material';
 import {
     EditingState,
     IntegratedEditing,
@@ -19,18 +21,17 @@ import {
     MonthView,
 } from '@devexpress/dx-react-scheduler-material-ui';
 
-import { Paper } from '@mui/material';
-
-import axios from "axios";
+import { FormControl, InputLabel, Select, MenuItem, Container } from '@mui/material';
 
 function Dashboard() {
-
-    const [currentDate, setCurrentDate] = useState('2023-12-01');
-
 
     const [appointments, setAppointments] = useState([]);
     const [loading, setLoading] = useState(true);
 
+    const [currentDate, setCurrentDate] = useState('2023-12-01');
+
+    const [currentStylist, setCurrentStylist] = useState(null);
+    const [stylist, setStylist] = useState([]);
 
 
     useEffect(() => {
@@ -58,10 +59,48 @@ function Dashboard() {
             setLoading(false);
         });
 
+        axios.get('https://f3lmrt7u96.execute-api.us-west-1.amazonaws.com/get_all_staff')
+        .then(response => {
+            setStylist(response.data);
+            setLoading(false);
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+            setLoading(false);
+        });
+
+        console.log(appointments);
+        console.log(stylist);
+
     },[]);
 
+    const CustomToolbar = () => (
+        <Toolbar.FlexibleSpace style={{ display: 'flex', alignItems: 'center' }}>
+          <FormControl style={{ marginRight: '20px', width: '150px' }}>
+            <Select
+              labelId="stylist-select-label"
+              id="stylist-select"
+              value={currentStylist || 'All'}  // Set initial value to 'All'
+              onChange={handleStylistChange}
+            >
+              <MenuItem value="All">
+                <em>All</em>
+              </MenuItem>
+              {stylist.map((stylistItem) => (
+                <MenuItem key={stylistItem.id} value={stylistItem.id}>
+                  {stylistItem.first_name + " "  + stylistItem.last_name}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </Toolbar.FlexibleSpace>
+    );
 
-
+    const handleStylistChange = (event) => {
+        const selectedStylistId = event.target.value;
+        // You can perform any additional actions when a stylist is selected
+        setCurrentStylist(selectedStylistId);
+    };
 
 
 
@@ -80,13 +119,15 @@ function Dashboard() {
 
                 <DayView startDayHour={9} endDayHour={18} />
                 <WeekView startDayHour={9} endDayHour={24} />
-                <MonthView startDayHour={12} endDayHour={20} />         
+                <MonthView startDayHour={12} endDayHour={20} />   
+
+                <Toolbar flexibleSpaceComponent={CustomToolbar}/>
+      
 
             </Scheduler>
 
 
         </Paper>
-
     )
 }
 
