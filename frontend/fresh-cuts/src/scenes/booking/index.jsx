dc7a7728dc0b0bb5d8ff954e3af51ae6683d2076
gweebyy <125400447+gweebyy@users.noreply.github.com>
2024-02-23 16:24:09 -0800

    Booking Page mobile view fix, Checkbox fix
    JIRA/DT 223-Customer Booking Page: Fix the mobile view
    
    JIRA/DT 224-Signup/Summary Pages: Salon policies and terms of service
    - Make it so user cannot signup or proceed with request for appointment until the checkbox is checked

--- frontend/fresh-cuts/src/scenes/booking/index.jsx
+++ frontend/fresh-cuts/src/scenes/booking/index.jsx
@@ -7,7 +7,7 @@ import ListItemText from '@mui/material/ListItemText';
 import OutlinedInput from '@mui/material/OutlinedInput';
 import Select from '@mui/material/Select';
 import {useState} from "react";
-import { Grid, Box, Container, Button, Typography, Card, CardContent } from '@mui/material';
+import { Grid, Box, Container, Button, Typography, Card, CardContent, Paper } from '@mui/material';
 import 'react-datepicker/dist/react-datepicker.css';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
@@ -39,22 +39,45 @@ const services = [
 ];*/
 
 const Booking = () => {
-  const [visible1, setVisible1] = useState(true);
-  const [visible2, setVisible2] = useState(true);
-  const [visible3, setVisible3] = useState(true);
-  const [visible4, setVisible4] = useState(true);
-  const toggleElement1 = () => {
-    setVisible1((prev) => !prev);
-  };
-  const toggleElement2 = () => {
-    setVisible2((prev) => !prev);
-  };
-  const toggleElement3 = () => {
-    setVisible3((prev) => !prev);
-  };
-  const toggleElement4 = () => {
-    setVisible4((prev) => !prev);
-  };
+  const [times, setTimes] = React.useState([
+    {id: 1, name: "9:00 AM"},
+    {id: 2, name: "9:30 AM"},
+    {id: 3, name: "10:00 AM"},
+    {id: 4, name: "10:30 AM"},
+    {id: 5, name: "11:00 AM"},
+    {id: 6, name: "11:30 AM"},
+    {id: 7, name: "12:00 PM"},
+    {id: 8, name: "12:30 PM"},
+    {id: 9, name: "1:00 PM"},
+    {id: 10, name: "1:30 PM"},
+    {id: 11, name: "2:00 PM"},
+    {id: 12, name: "2:30 PM"},
+    {id: 13, name: "3:00 PM"},
+    {id: 14, name: "3:30 PM"},
+    {id: 15, name: "4:00 PM"},
+    {id: 16, name: "4:30 PM"},
+    {id: 17, name: "5:00 PM"},
+    {id: 18, name: "5:30 PM"},
+    {id: 19, name: "6:00 PM"},
+    {id: 20, name: "6:30 PM"},
+    {id: 21, name: "7:00 PM"},
+    {id: 22, name: "7:30 PM"},
+  ])
+  function removeTime(id) {
+    const newTimes = times.filter((l) => l.id !== id);
+    setTimes(newTimes);
+  }
+
+  function revealNoApt() {
+    document.getElementById("noApt").innerHTML = "Sorry, there are no available appointments, please try another date.";
+    document.getElementById("callUs").innerHTML = "Call to see if there are any last minute openings at (916) 451-1517";
+  }
+
+  function hideNoApt() {
+    document.getElementById("noApt").innerHTML = "";
+    document.getElementById("callUs").innerHTML = "";
+  }
+
   const [staffData, setStaffData] = useState(null);
   const [startDate, setStartDate] = useState(new Date());
   const [service, setService] = React.useState([]);
@@ -89,20 +112,56 @@ const Booking = () => {
   const handleSearchClick = async () => {
     const data = await fetchData(stylist); // Assuming `stylist` holds the staff_id you want to fetch
     setStaffData(data); // Update the state with the fetched data
+    
+    //Reset times
+    const newTimes = [
+      {id: 1, name: "9:00 AM"},
+      {id: 2, name: "9:30 AM"},
+      {id: 3, name: "10:00 AM"},
+      {id: 4, name: "10:30 AM"},
+      {id: 5, name: "11:00 AM"},
+      {id: 6, name: "11:30 AM"},
+      {id: 7, name: "12:00 PM"},
+      {id: 8, name: "12:30 PM"},
+      {id: 9, name: "1:00 PM"},
+      {id: 10, name: "1:30 PM"},
+      {id: 11, name: "2:00 PM"},
+      {id: 12, name: "2:30 PM"},
+      {id: 13, name: "3:00 PM"},
+      {id: 14, name: "3:30 PM"},
+      {id: 15, name: "4:00 PM"},
+      {id: 16, name: "4:30 PM"},
+      {id: 17, name: "5:00 PM"},
+      {id: 18, name: "5:30 PM"},
+      {id: 19, name: "6:00 PM"},
+      {id: 20, name: "6:30 PM"},
+      {id: 21, name: "7:00 PM"},
+      {id: 22, name: "7:30 PM"},
+    ];
+    setTimes(newTimes);
+
+    //Handle interaction with the removeTime() function based on availability
+
+    if(times.length != 0) { //Reveal no available appointments if our list of times is empty
+      hideNoApt();
+    }
+    else {
+      revealNoApt();
+    }
   };
+  
   return (
     <Container maxWidth="100%">
         <Grid container spacing={2} style={{minHeight: "500px"}}>
           <Grid item xs={12}>
             <Grid container spacing={2}>
               <Grid item xs={12}>
-                <h1 align="left" style={{marginLeft: 50, marginTop: 50}}> Schedule an Appointment </h1>
+                <h1 align="left" style={{marginLeft: 20, marginTop: 50}}> Schedule an Appointment </h1>
               </Grid>
             </Grid>
-            <div style={{borderTop: "2px solid", color: "black", marginLeft: 50, maxWidth:"80%"}}/>
+            <div style={{borderTop: "2px solid", color: "black", marginLeft: 20, maxWidth:"80%"}}/>
             <Grid container>
-              {visible1 && (
-              <Card style={{ height: "30%", width: "10%", marginLeft: 50, marginTop: 20, backgroundColor: "#f2f2f2", display: 'flex', flexDirection: 'column' }}>
+              <Card style={{ height: "30%", width: "10%", minWidth: "180px", marginLeft: 20, marginTop: 20, backgroundColor: "#f2f2f2", display: 'flex', flexDirection: 'column' }}>
                 <CardContent>
                   <FormControl fullWidth>
                   <InputLabel id="stylist-select-label">Select a Stylist</InputLabel>
@@ -122,9 +181,7 @@ const Booking = () => {
                   </FormControl>
                 </CardContent>
               </Card>
-              )}
-              {visible2 && (
-              <Card style={{ height: "30%", width: "30%", marginLeft: 20, marginTop: 20, backgroundColor: "#f2f2f2", display: 'flex', flexDirection: 'column' }}>
+              <Card style={{ height: "30%", width: "30%", minWidth: "180px", marginLeft: 20, marginTop: 20, backgroundColor: "#f2f2f2", display: 'flex', flexDirection: 'column' }}>
                 <CardContent>
                   <FormControl fullWidth>
                     <InputLabel id="service-select-label">Select One or More Services</InputLabel>
@@ -149,9 +206,7 @@ const Booking = () => {
                   </FormControl>
                 </CardContent>
               </Card>
-              )}
-              {visible3 && (
-              <Card style={{ height: "30%", width: "10%", marginLeft: 20, marginTop: 20, backgroundColor: "#f2f2f2", display: 'flex', flexDirection: 'column' }}>
+              <Card style={{ height: "30%", width: "10%", minWidth:"146px", padding: 17, marginLeft: 20, marginTop: 20, backgroundColor: "#f2f2f2", display: 'flex', flexDirection: 'column' }}>
                 <CardContent>
                   <FormControl fullWidth>
                   <LocalizationProvider dateAdapter={AdapterDayjs}>
@@ -169,9 +224,7 @@ const Booking = () => {
                   </FormControl>
                 </CardContent>
               </Card>
-              )}
-              {visible4 && (
-              <Card style={{ height: "30%", width: "10%", marginLeft: 20, marginTop: 20, backgroundColor: "#f2f2f2", display: 'flex', flexDirection: 'column' }}>
+              <Card style={{ height: "30%", width: "10%", minWidth:"180px", marginLeft: 20, marginTop: 20, backgroundColor: "#f2f2f2", display: 'flex', flexDirection: 'column' }}>
                 <CardContent>
                   <FormControl fullWidth>
                     <Button variant='Contained'
@@ -185,41 +238,43 @@ const Booking = () => {
                   </FormControl>
                 </CardContent>
               </Card>
-              )}
             </Grid>
           </Grid>
         </Grid>
 
-        <Grid container spacing={2} style={{minHeight: "300px"}} marginLeft='50px'>
-          <Stack flexDirection='column' margin='20px'>
-          <Grid item xs={12}><Typography variant='h5'>Date Time</Typography></Grid>
-          <Grid item xs={12}><Typography variant='h6'>Morning</Typography></Grid>
-          <Grid item xs={12}>
-            <Button variant='Contained'
-                type='submit'
-                style={{
-                  backgroundColor: "#e95252",
-                  padding: "10px 20px"
-                }}>
-                <Typography color='white'>AM</Typography>
-              </Button>
-          </Grid>
-
-          <Grid item xs={12}><Typography variant='h6'>Afternoon</Typography></Grid>
-          <Grid item xs={12}>
-            <Button variant='Contained'
-                type='submit'
-                style={{
-                  backgroundColor: "#e95252",
-                  padding: "10px 20px"
-                }}>
-                <Typography color='white'>PM</Typography>
-              </Button>
-          </Grid>
+        <Grid container spacing={2} style={{minHeight: "300px"}} marginLeft='20px'>
+          <Stack flexDirection='column' marginTop='50px'>
+            <Grid item xs={12}><Typography variant='h5' marginLeft='50px'>Appointment Time</Typography></Grid>
+            <Grid item xs={8}>
+              <div style={{display: 'flex'}}>
+                <ul>
+                  {
+                    times.map((time) => {
+                      return <Button variant='Contained'
+                      id={time.id}
+                      type='submit'
+                      onClick={()=> removeTime(time.id)}
+                      style={{
+                        backgroundColor: "#e95252",
+                        width: "120px",
+                        padding: "10px 10px",
+                        margin: "10px",
+                      }}>
+                      <Typography color='white'>{time.name}</Typography>
+                    </Button>
+                    })
+                  }
+                </ul>
+              </div>
+              <script>
+              document.getElementById("p1").innerHTML = "New text!";
+              </script>
+              <h1 id="noApt" align="center" style={{width: "70%", marginTop: 0}}></h1><br/>
+              <p id="callUs" align="center" style={{width: "70%", marginTop: 0}}></p>
+            </Grid>
           </Stack>
         </Grid>
     </Container>
   );
 }
-
 export default Booking;
