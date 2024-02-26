2682be32daada416427da2a4ea69dcf8d0326285
acn916 <austin.n916@gmail.com>
2024-02-25 18:19:45 -0700

    Removed duplicate time state variable

--- frontend/fresh-cuts/src/scenes/booking/index.jsx
+++ frontend/fresh-cuts/src/scenes/booking/index.jsx
@@ -78,30 +78,6 @@ const Booking = () => {
     document.getElementById("callUs").innerHTML = "";
   }
 
-  const [times, setTimes] = React.useState([
-    {id: 1, name: "9:00 AM"},
-    {id: 2, name: "9:30 AM"},
-    {id: 3, name: "10:00 AM"},
-    {id: 4, name: "10:30 AM"},
-    {id: 5, name: "11:00 AM"},
-    {id: 6, name: "11:30 AM"},
-    {id: 7, name: "12:00 PM"},
-    {id: 8, name: "12:30 PM"},
-    {id: 9, name: "1:00 PM"},
-    {id: 10, name: "1:30 PM"},
-    {id: 11, name: "2:00 PM"},
-    {id: 12, name: "2:30 PM"},
-    {id: 13, name: "3:00 PM"},
-    {id: 14, name: "3:30 PM"},
-    {id: 15, name: "4:00 PM"},
-    {id: 16, name: "4:30 PM"},
-    {id: 17, name: "5:00 PM"},
-    {id: 18, name: "5:30 PM"},
-    {id: 19, name: "6:00 PM"},
-    {id: 20, name: "6:30 PM"},
-    {id: 21, name: "7:00 PM"},
-    {id: 22, name: "7:30 PM"},
-  ])
   function removeTime(id) {
     const newTimes = times.filter((l) => l.id !== id);
     setTimes(newTimes);
