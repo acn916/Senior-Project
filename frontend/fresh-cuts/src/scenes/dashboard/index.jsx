f8323e021e3a7aecee34577653f522bed3aae670
Laura Kay <laurakay@Lauras-MacBook-Pro.local>
2024-03-06 17:24:29 -0800

    Fixed error, missing values

--- frontend/fresh-cuts/src/scenes/dashboard/index.jsx
+++ frontend/fresh-cuts/src/scenes/dashboard/index.jsx
@@ -90,7 +90,7 @@ function Dashboard() {
     const fetchClients = async () => {
       try {
           const clientResponse = await axios.get('https://f3lmrt7u96.execute-api.us-west-1.amazonaws.com/client');
-          setClients(clientResponse.data);
+          
       } catch (error) {
           console.error("Error fetching clients:", error);
       }
