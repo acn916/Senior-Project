223f72f3842f19e69912827b65cebbc69bcc00c1
bdam98 <brandondam10@gmail.com>
2024-03-08 23:05:22 -0800

    # JIRA 231, 232, 233, 234 Request Page Clicking the rebook button will bring a popup to edit information. Small formatting changes to Accept and Decline buttons.

--- frontend/fresh-cuts/src/scenes/request/index.jsx
+++ frontend/fresh-cuts/src/scenes/request/index.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Grid } from '@mui/material';
+import { Grid, Button } from '@mui/material';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -14,43 +14,43 @@ import axios from "axios";
 
 export const rows = [ 
   {id: 1,
-  confirmation_timestamp:'02/10/2023',
-  scheduled_at: "2023/09/17", 
+  confirmation_timestamp:'02/10/2023 8:00:00',
+  scheduled_at: "2023/09/17 9:00:00", 
   status:"Pending",
   service_id: 2,
   client_id: 1},
 
   {id: 2,
-  confirmation_timestamp:'02/10/2023',
-  scheduled_at: "2023/09/17", 
+  confirmation_timestamp:'02/10/2023 8:00:00',
+  scheduled_at: "2023/09/17 9:00:00", 
   status:"Pending",
   service_id: 15,
   client_id: 1},
 
   {id: 3,
-  confirmation_timestamp:'02/10/2023',
-  scheduled_at: "2023/09/17", 
+  confirmation_timestamp:'02/10/2023 8:00:00',
+  scheduled_at: "2023/09/17 9:00:00", 
   status:"Pending",
   service_id: 7,
   client_id: 1},
 
   {id: 4,
-  confirmation_timestamp:'02/10/2023',
-  scheduled_at: "2023/09/17", 
+  confirmation_timestamp:'02/10/2023 8:00:00',
+  scheduled_at: "2023/09/17 9:00:00", 
   status:"Pending",
   service_id: 10,
   client_id: 1},
 
   {id: 5,
-  confirmation_timestamp:'02/10/2023', 
-  scheduled_at: "2023/09/17", 
+  confirmation_timestamp:'02/10/2023 8:00:00', 
+  scheduled_at: "2023/09/17 9:00:00", 
   status:"Pending",
   service_id: 27,
   client_id: 1},
 
   {id: 6,
-  confirmation_timestamp:'02/10/2023',
-  scheduled_at: "2023/09/17", 
+  confirmation_timestamp:'02/10/2023 8:00:00',
+  scheduled_at: "2023/09/17 9:00:00", 
   status:"Pending",
   service_id: 5,
   client_id: 2},
@@ -101,14 +101,21 @@ export const rows3 = [
 
 const Request = () => { 
 
- // const [requests, setRequests] = useState(rows.filter(test => test.status === "Pending")); //with data above (fake data)
-  //const [services, setServices] = useState(rows2); //with data above (fake data)
-  //const [clients, setClients] = useState(rows3); //with data above (fake data)
+  /*const [requests, setRequests] = useState(rows.filter(test => test.status === "Pending")); //with data above (fake data)
+  const [services, setServices] = useState(rows2); //with data above (fake data)
+  const [clients, setClients] = useState(rows3); //with data above (fake data)
+  const [names, setNames] = useState(requests.map(request => ({
+    id: request.id,
+    client_id: request.client_id,
+    first_name: findName(clients, request.client_id).first_name,
+    last_name: findName(clients, request.client_id).last_name,
+  })))*/
   
   const [requests, setRequests] = useState([]); //for get API (actual request data) //entire commented block is functional and will be used for database info
   const [services, setServices] = useState([]); //for get API (actual service data)
   const [clients, setClients] = useState([]); //for get API (actual client data) 
   const [loading, setLoading] = useState(true);
+  const [names, setNames] = useState([]);
 
   const fetchAppointments = async () =>  {
     try{
@@ -116,7 +123,7 @@ const Request = () => {
       
       const pending_request = response.data.filter(appointment => appointment.status === 'Pending');
       setRequests(pending_request);
-
+      
     } catch(error){
       console.error("Error fetching data:'", error);
       setLoading(false);
@@ -143,10 +150,20 @@ const Request = () => {
         console.error('Error fetching data:', error);
         setLoading(false);
       });
-
+    
     axios.get('https://f3lmrt7u96.execute-api.us-west-1.amazonaws.com/client')
       .then(response => {
         setClients(response.data);
+        axios.get('https://f3lmrt7u96.execute-api.us-west-1.amazonaws.com/appointments')
+          .then(response2 => {
+             const pending_request = response2.data.filter(appointment => appointment.status === 'Pending')
+             setNames(pending_request.map(request => ({
+              id: request.id,
+              client_id: request.client_id,
+              first_name: response.data.find(client => client.id === request.client_id).first_name,
+              last_name: response.data.find(client => client.id === request.client_id).last_name
+            })))
+          })
       })
       .catch(error => {
         console.error('Error fetching data:', error);
@@ -158,7 +175,6 @@ const Request = () => {
       const intervalId = setInterval(fetchAppointments, 9000);
 
       return () => clearInterval(intervalId);  
-
   }, []);
   
   //changes status to Confirmed
@@ -173,31 +189,67 @@ const Request = () => {
     ];
 
     editAppointment(id, updatedRequest);
-
     setRequests(newRequests);
+    handleDelete(id);
   }
 
   //changes status to Denied
   const handleDeny = (id) => {
     const currentRequestIndex = requests.findIndex((request) => request.id === id);
     const updatedRequest = {...requests[currentRequestIndex], status: "Cancelled"};
+    console.log("updated request", updatedRequest);
     const newRequests = [
       ...requests.slice(0, currentRequestIndex),
       updatedRequest,
       ...requests.slice(currentRequestIndex + 1)
     ];
-    editAppointment(id, updatedRequest);
 
+    editAppointment(id, updatedRequest);
     setRequests(newRequests);
+    handleDelete(id);
   }
 
   //deletes the row from the array
   const handleDelete = (id) => {
-    handleDeny(id)
     setRequests(values => {
       return values.filter(request => request.id !== id)
     })
+    setNames(values => {
+      return values.filter(name => name.id !== id)
+    })
   }
+
+  //updates the service id and date. Name is also updated (needs to be fixed to permanently update)
+  const handleRebook = (id, service, dateTime, first_name, last_name) => {
+    const currentRequestIndex = requests.findIndex((request) => request.id === id);
+    const updatedRequest = {...requests[currentRequestIndex], service_id: service, scheduled_at: dateTime};
+    const newRequests = [
+      ...requests.slice(0, currentRequestIndex),
+      updatedRequest,
+      ...requests.slice(currentRequestIndex + 1)
+    ];
+
+    const currentNameIndex = names.findIndex((request) => request.id === id);
+    const updatedName = {...names[currentNameIndex], first_name: first_name, last_name: last_name};
+    const newNames = [
+      ...names.slice(0, currentNameIndex),
+      updatedName,
+      ...names.slice(currentNameIndex + 1)
+    ];
+
+    editAppointment(id, updatedRequest);
+    console.log("updated request", updatedRequest);
+    console.log("updated request", updatedName);
+
+    setRequests(newRequests);
+    setNames(newNames);
+  }
+  
+  /*function findName(array, otherID) {
+    return array.find((value) => {
+      return value.id == otherID;
+    })
+  } may be used in future*/
 
   return (
     <Grid>
@@ -224,34 +276,48 @@ const Request = () => {
               >
                 <TableCell component="th" scope="row"> {request.confirmation_timestamp}</TableCell>
 
-                {clients.map((client) => request.client_id === client.id ? (
-                  <React.Fragment key={client.id}>
-                    <TableCell align="left" sx={{color: "#2F65DD"}}>
-                      <AccountCircleIcon sx={{marginBottom: -1}}/> {' ' + client.first_name + " " + client.last_name}
-                    </TableCell>
-                  </React.Fragment>
-                ): null)}
-
-                {services.map((service) => request.service_id === service.id ? (
-                  <React.Fragment key={service.id}>
-                    <TableCell align="left">{service.name}</TableCell>
-                    <TableCell align="left">{'$'+ service.price}</TableCell>
-                  </React.Fragment>
-                ): null)}
+                <TableCell align="left" sx={{color: "#2F65DD"}}>
+                  {names.map((name) => request.id == name.id ? (
+                    <React.Fragment key={name.id}>
+                      <AccountCircleIcon sx={{marginBottom: -1}}/> {' ' + name.first_name + " " + name.last_name}
+                    </React.Fragment>
+                  ): null)}
+                </TableCell>
+                  
+                <TableCell align="left">
+                  {services.map((service) => request.service_id == service.id ? (
+                    <React.Fragment key={service.id}>
+                      {service.name}
+                    </React.Fragment>
+                  ): null)}
+                </TableCell>
+                    
+                <TableCell align="left">
+                  {services.map((service) => request.service_id == service.id ? (
+                    <React.Fragment key={service.id}>
+                      {'$'+ service.price}
+                    </React.Fragment>
+                  ): null)}
+                </TableCell>
                 
                 <TableCell align="left">{request.scheduled_at}</TableCell>
                 <TableCell align="left">
                   <Accept id={request.id} handleConfirm={handleConfirm}/>
                   <span>|</span>
-                  <Decline id={request.id} handleDelete={handleDelete}/>
+                  <Decline id={request.id} handleDeny={handleDeny}/>
                   <span>|</span>
-                  <Rebook/>
+                  <Rebook 
+                    id={request.id} 
+                    passedNames={names}
+                    service_id={request.service_id}
+                    scheduled_at={request.scheduled_at}
+                    handleRebook={handleRebook}
+                  />
                 </TableCell>
               </TableRow>
             ))}
           </TableBody>
         </Table>
-
       </TableContainer>
     </Grid>
   );
