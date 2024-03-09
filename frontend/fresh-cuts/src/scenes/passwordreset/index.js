4e32d0ae15d557786ba19a3be6e7e7c3bc424796
Ben <voben21@gmail.com>
2024-03-08 19:42:20 -0800

    DT 220 when forgot password is submitted, email is sent out with verification code, need to redirect user to a form to submit code + new password

--- frontend/fresh-cuts/src/scenes/passwordreset/index.js
+++ frontend/fresh-cuts/src/scenes/passwordreset/index.js
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Grid, Paper, TextField, Button, Typography } from '@mui/material'
 import { Link } from 'react-router-dom'
 
+import { CognitoUser, CognitoUserPool } from "amazon-cognito-identity-js";
+import Pool from "../login/UserPool";
+
 export default function Passwordreset() {
     const paperStyle={padding:20, height:'350px', width:'90%', maxWidth:'350px', margin:"30px auto"}
+
+    const [stage, setStage] = useState(1); // 1 = email stage, 2 = code stage
+    const [email, setEmail] = useState("");
+    const [code, setCode] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+  
+    const getUser = () => {
+      return new CognitoUser({
+        Username: email.toLowerCase(),
+        Pool,
+      });
+    };
+
+
+    const sendCode = (event) => {
+        event.preventDefault();
+    
+        getUser().forgotPassword({
+          onSuccess: (data) => {
+            console.log("onSuccess:", data);
+          },
+          onFailure: (err) => {
+            console.error("onFailure:", err);
+          },
+          inputVerificationCode: (data) => {
+            console.log("Input code:", data);
+            setStage(2);
+          },
+        });
+      };
+
 
     return (
         <>
@@ -25,22 +60,26 @@ export default function Passwordreset() {
                 </Grid>
 
                 <Typography align='center'>Enter your email to get a link to get back into your account.</Typography>
+                <form onSubmit={sendCode}>
 
-                <TextField
-                    required id="email-pwreset"
-                    label="Email Address"
-                    margin="normal"
-                    fullWidth
-                />
+                    <TextField
+                        required id="email-pwreset"
+                        label="Email Address"
+                        margin="normal"
+                        fullWidth
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        />
 
-                <Button variant='Contained'
-                        style={{
-                        backgroundColor: "#e95252",
-                        padding: "12px 24px"
-                        }}
-                        fullWidth required>
-                    <Typography color='white'>Proceed</Typography>
-                </Button>
+                    <Button type="submit" variant='Contained'
+                            style={{
+                                backgroundColor: "#e95252",
+                                padding: "12px 24px"
+                            }}
+                            fullWidth required>
+                        <Typography color='white'>Proceed</Typography>
+                    </Button>
+                </form>
             </Paper>
         </Grid>
         </>
